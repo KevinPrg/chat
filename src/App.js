bc036649962e4a3ce1b88e611b7e2716ee2f0373
psycho33200 <pininfarina47@gmail.com>
2019-10-08 17:53:17 +0200

    update chat

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Contact from './components/Contact';
-
-
-const Kevin = {
-  avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX34l5Vy7K3aMXNcl8V7ncTVf8Nwfsic6O9FOkwEYKw4-zwLLh&s',
-  name: 'Kevin Parage',
-  online: true
-}
-const Corinne = {
-  avatar: 'http://www.moving-up.fr/wp-content/uploads/2017/05/corinneleycharles.jpg',
-  name: 'Corrine Russo',
-  online: false
-}
-const Juliette = {
-  avatar: 'https://www.babelio.com/users/AVT_Juliette-Lemaitre_8819.jpg',
-  name: 'Juliette Boyrie',
-  online: true
-}
+import ContactLis from './components/ContactList';
 
 
 function App() {
   return (
     <div className="App">
-      <Contact{...Kevin}/>
-      <Contact{...Corinne}/>
-      <Contact{...Juliette}/>
+      <ContactLis />
     </div>
   );
 }
